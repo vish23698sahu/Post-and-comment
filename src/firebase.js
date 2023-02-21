39f97809d6361043c4bb2d332b0ebdb39f7e6f0f
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-02-21 19:25:00 +0530

    v - 35 .env setup

--- src/firebase.js
+++ src/firebase.js
@@ -3,20 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
-import { API_KEY } from './APIKeys'
-import { AUTH_DOMAIN } from './APIKeys'
-import { PROJECT_ID } from './APIKeys'
-import { STORAGE_BUCKET } from './APIKeys'
-import { MESSAGING_SENDER_ID } from './APIKeys'
-import { APP_ID } from './APIKeys'
-
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
