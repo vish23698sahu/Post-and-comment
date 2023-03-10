bff9be03539f880d21599efd7711fb0178e46489
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-03-10 23:52:36 +0530

    v - 38 footer 2023

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './style.css';
 
-export default function Footer () {
+export default function Footer() {
   return (
     <div>
-        <p className='footer'>@Go Social 2022.</p>
+      <p className='footer'>@Go Social 2023.</p>
     </div>
   )
 }
